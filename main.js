da8a38864885054d22ec27c4a89709808d3a7bea
Edgar Ortiz <ojedgar108@gmail.com>
2024-05-09 15:32:56 -0500

    First two functions on js

---
+++ main.js
@@ -0,0 +1,10 @@
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal";
+}
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+console.log(favoriteAnimal("cats"));
+console.log(sum(5, 7));
