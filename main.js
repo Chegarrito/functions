02d68c62c96fed29e8e42aca369afa012e34635e
Edgar Ortiz <ojedgar108@gmail.com>
2024-05-09 16:54:06 -0500

    Now is added lol, im learning xd

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function favoriteAnimal(animal) {
+/* function favoriteAnimal(animal) {
     return animal + " is my favorite animal";
 }
 
@@ -7,4 +7,31 @@ function sum(num1, num2){
 }
 
 console.log(favoriteAnimal("cats"));
-console.log(sum(5, 7));
+console.log(sum(5, 7)); */
+/* 
+function showCount(count) {
+    alert(count ?? "unknown");
+}
+
+showCount(0);
+showCount(null);
+showCount(); */
+/* 
+function sayHi(){
+    alert("Hi!");
+}
+
+let func = sayHi;
+
+alert(sayHi());
+alert(func());
+ */
+
+let sayHi = function() {
+    alert("HI!");
+};
+
+let func = sayHi;
+
+sayHi();
+func();
