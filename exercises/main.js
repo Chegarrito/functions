d13a75595272dcbd262148339645611115c435f4
Edgar Ortiz <ojedgar108@gmail.com>
2024-05-11 00:48:41 -0500

    Add function lastLetter

--- exercises/main.js
+++ exercises/main.js
@@ -12,9 +12,18 @@ function multiply(a, b) {
 
 console.log(multiply(2, 5));
 
+// Capitalize the first letter of a word function
 function capitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
      
 }
 
-console.log(capitalize("edgar"));
+console.log(capitalize("s"));
+
+// Function that takes a string and returns the very last letter of that string
+
+function lastLetter(word2) {
+    return word2[word2.length - 1];
+}
+
+console.log(lastLetter("abcd"));
